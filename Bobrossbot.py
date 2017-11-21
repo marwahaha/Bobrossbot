07bc7e22a07d418653b3cd6c14efb2c995cd9d2a
whaliam <33747409+whaliam@users.noreply.github.com>
2017-11-20 21:48:11 -0600

    Update Bobrossbot.py
    Bob should now only respond to the string("Bob Ross") Ignoring the case.

--- Bobrossbot.py
+++ Bobrossbot.py
@@ -1,155 +1,155 @@
-# BobRossBot_ v0.4 (added loop and user input for n)
+# BobRossBot_ v0.3 (cleaned up code)
 # Created by /u/whaliam
 # 6Ep96ck9@protonmail.com
 # Import required libaries
-n = int(input('Enter Number of times to repeat'))
-for i in range(n):
-    print("loading required libs")
 
-    import praw
-    import pdb
-    import re
-    import os
-    import time
-    import random
-    import sys
-    from datetime import datetime
+print("loading required libs")
 
-    # Create Reddit instance
-    # Setting variables
-    print("logging in")
-    reddit = praw.Reddit(user_agent='<useragent>',
-                         client_id='<clientid>',
-                         client_secret='<clientsecret>',
-                         username='<username>',
-                         password='<password>
-    print("welcome BobRossBot_ :)")
+import praw
+import pdb
+import re
+import os
+import time
+import random
+import sys
+from datetime import datetime
 
-    # Enter quotes in the string below
-    print("loading quotes")
+# Create Reddit instance
+# Setting variables
+print("logging in")
+reddit = praw.Reddit(user_agent='Bob Ross Bot v0.1',
+                  client_id='<clientid>',
+                  client_secret='<clientsecret>',
+                  username='<username>',
+                  password='<password>')
 
-    ross_quotes = \
-        [
-            " Trees cover up a multitude of sins.",
+print("welcome BobRossBot_ :)")
 
-            " Remember our Golden Rule: A thin paint sticks to a thick paint. ",
+#Enter quotes in the string below
+print("loading quotes")
 
-            " And that makes it look like birch trees, isn't that sneaky? Heh. Ha. It's gorgeous. ",
+ross_quotes = \
+[
+" Trees cover up a multitude of sins.",
 
-            " Be sure to use odorless paint-thinner. If it's not odorless, you'll find yourself working alone very, very quick. ",
+" Remember our Golden Rule: A thin paint sticks to a thick paint. ",
 
-            " Let's just blend this little rascal here, ha! Happy as we can be. ",
+" And that makes it look like birch trees, isn't that sneaky? Heh. Ha. It's gorgeous. ",
 
-            " Clouds are very, very free. ",
+" Be sure to use odorless paint-thinner. If it's not odorless, you'll find yourself working alone very, very quick. ",
 
-            " Decide where your little footy hills live. ",
+" Let's just blend this little rascal here, ha! Happy as we can be. ",
 
-            " Shwooop. Hehe. You have to make those little noises, or it just doesn't work. ",
+" Clouds are very, very free. ",
 
-            " Try to imagine that you are a tree. How do you want to look out here? ",
+" Decide where your little footy hills live. ",
 
-            "  You know me, I gotta put in a big tree. ",
+" Shwooop. Hehe. You have to make those little noises, or it just doesn't work. ",
 
-            " Gotta give him a friend. Like I always say 'everyone needs a friend'. ",
+" Try to imagine that you are a tree. How do you want to look out here? ",
 
-            "  Any time ya learn, ya gain. ",
+"  You know me, I gotta put in a big tree. ",
 
-            "  Just put a few do-ers in there... ",
+" Gotta give him a friend. Like I always say 'everyone needs a friend'. ",
 
-            "  Maybe in our world there lives a happy little tree over there. ",
+"  Any time ya learn, ya gain. ",
 
-            "  You can do anything you want to do. This is your world. ",
+"  Just put a few do-ers in there... ",
 
-            "  You can put as many or as few as you want in your world. ",
+"  Maybe in our world there lives a happy little tree over there. ",
 
-            " That's a crooked tree. We'll send him to Washington. ",
+"  You can do anything you want to do. This is your world. ",
 
-            " I like to beat the brush. ",
+"  You can put as many or as few as you want in your world. ",
 
-            " In painting, you have unlimited power. You have the ability to move mountains. You can bend rivers. But when I get home, the only thing I have power over is the garbage. ",
+" That's a crooked tree. We'll send him to Washington. ",
 
-            " You need the dark in order to show the light. ",
+" I like to beat the brush. ",
 
-            " Look around. Look at what we have. Beauty is everywhere—you only have to look to see it. ",
+" In painting, you have unlimited power. You have the ability to move mountains. You can bend rivers. But when I get home, the only thing I have power over is the garbage. ",
 
-            " There's nothing wrong with having a tree as a friend. ",
+" You need the dark in order to show the light. ",
 
-            " There's nothing wrong with having a tree as a friend. ",
+" Look around. Look at what we have. Beauty is everywhere—you only have to look to see it. ",
 
-            " They say everything looks better with odd numbers of things. But sometimes I put even numbers—just to upset the critics. ",
+" There's nothing wrong with having a tree as a friend. ",
 
-            " How do you make a round circle with a square knife? That’s your challenge for the day. ",
+" There's nothing wrong with having a tree as a friend. ",
 
-            " How do you make a round circle with a square knife? That’s your challenge for the day. ",
+" They say everything looks better with odd numbers of things. But sometimes I put even numbers—just to upset the critics. ",
 
-            " I remember when my Dad told me as a kid, ‘If you want to catch a rabbit, stand behind a tree and make a noise like a carrot. Then when the rabbit comes by you grab him.’ Works pretty good until you try to figure out what kind of noise a carrot makes… ",
+" How do you make a round circle with a square knife? That’s your challenge for the day. ",
 
-            " We tell people sometimes: we're like drug dealers, come into town and get everybody absolutely addicted to painting. It doesn't take much to get you addicted. ",
+" How do you make a round circle with a square knife? That’s your challenge for the day. ",
 
-            " The secret to doing anything is believing that you can do it. Anything that you believe you can do strong enough, you can do. Anything. As long as you believe. ",
+" I remember when my Dad told me as a kid, ‘If you want to catch a rabbit, stand behind a tree and make a noise like a carrot. Then when the rabbit comes by you grab him.’ Works pretty good until you try to figure out what kind of noise a carrot makes… ",
 
-            " Water's like me. It's laaazy ... Boy, it always looks for the easiest way to do things ",
+" We tell people sometimes: we're like drug dealers, come into town and get everybody absolutely addicted to painting. It doesn't take much to get you addicted. ",
 
-            " Oooh, if you have never been to Alaska, go there while it is still wild. My favorite uncle asked me if I wanted to go there, Uncle Sam. He said if you don't go, you're going to jail. That is how Uncle Sam asks you. ",
+" The secret to doing anything is believing that you can do it. Anything that you believe you can do strong enough, you can do. Anything. As long as you believe. ",
 
-            " I really believe that if you practice enough you could paint the 'Mona Lisa' with a two-inch brush. ",
+" Water's like me. It's laaazy ... Boy, it always looks for the easiest way to do things ",
 
-            " If I paint something, I don't want to have to explain what it is. ",
+" Oooh, if you have never been to Alaska, go there while it is still wild. My favorite uncle asked me if I wanted to go there, Uncle Sam. He said if you don't go, you're going to jail. That is how Uncle Sam asks you. ",
 
-            " We artists are a different breed of people. We're a happy bunch. ",
+" I really believe that if you practice enough you could paint the 'Mona Lisa' with a two-inch brush. ",
 
-            " We don't make mistakes. We just have happy accidents. ",
+" If I paint something, I don't want to have to explain what it is. ",
 
-            " We don't make mistakes. We just have happy accidents. ",
+" We artists are a different breed of people. We're a happy bunch. ",
 
-            " You too can paint almighty pictures. ",
+" We don't make mistakes. We just have happy accidents. ",
 
-            " Don’t forget to make all these little things individuals — all of them special in their own way. ",
+" We don't make mistakes. We just have happy accidents. ",
 
-            " Talent is a pursued interest. Anything that you’re willing to practice, you can do. ",
+" You too can paint almighty pictures. ",
 
-            " Make love to the canvas. ",
+" Don’t forget to make all these little things individuals — all of them special in their own way. ",
 
-            " Don’t forget to tell these special people in your life just how special they are to you. ",
+" Talent is a pursued interest. Anything that you’re willing to practice, you can do. ",
 
-            " Just let go — and fall like a little waterfall. ",
+" Make love to the canvas. ",
 
-            " You can do anything you want to do. This is your world. ",
+" Don’t forget to tell these special people in your life just how special they are to you. ",
 
-            " You can have anything you want in the world — once you help everyone around you get what they want. ",
+" Just let go — and fall like a little waterfall. ",
 
-            " If you do too much, it’s going to lose its effectiveness. ",
+" You can do anything you want to do. This is your world. ",
 
-            " This is happy place; little squirrels live here and play. ",
+" You can have anything you want in the world — once you help everyone around you get what they want. ",
 
-            " Remember how free clouds are. They just lay around in the sky all day long ",
+" If you do too much, it’s going to lose its effectiveness. ",
 
-            " That’s where the crows will sit. But we’ll have to put an elevator to put them up there because they can’t fly, but they don’t know that, so they still try. ",
+" This is happy place; little squirrels live here and play. ",
 
-            " We don’t really know where this goes — and I’m not sure we really care. ",
+" Remember how free clouds are. They just lay around in the sky all day long ",
 
-            " Go out on a limb — that’s where the fruit is. ",
+" That’s where the crows will sit. But we’ll have to put an elevator to put them up there because they can’t fly, but they don’t know that, so they still try. ",
 
-            " Isn’t it fantastic that you can change your mind and create all these happy things? ",
+" We don’t really know where this goes — and I’m not sure we really care. ",
 
-            " It’s life. It’s interesting. It’s fun. ",
+" Go out on a limb — that’s where the fruit is. ",
 
-        ]
-    username = "BobRossBot_"
-    # Set subreddit parameters
-    subreddit = reddit.subreddit("all")
-    ross_reply = random.choice(ross_quotes)
+" Isn’t it fantastic that you can change your mind and create all these happy things? ",
 
-    # Set comment parameters
-    for comment in subreddit.stream.comments():
-        if re.search("mistake", comment.body, re.IGNORECASE):
-            print("comment found!")
-            print("preparing quote")
-            ross_reply = random.choice(ross_quotes)
-            comment.reply(ross_reply)
-            print("replied with quote!")
-            print("starting cooldown")
-            print("cooldown")
-            time.sleep(4)
+" It’s life. It’s interesting. It’s fun. ",
+
+]
+username = "BobRossBot_"
+# Set subreddit parameters
+subreddit = reddit.subreddit("all")
+ross_reply = random.choice(ross_quotes)
+
+# Set comment parameters
+for comment in subreddit.stream.comments():
+    if re.search("Bob Ross", comment.body, re.IGNORECASE):
+        print("comment found!")
+        print("preparing quote")
+        ross_reply = random.choice(ross_quotes)
+        comment.reply(ross_reply)
+        print("replied with quote!")
+        print("starting cooldown")
+        print("cooldown")
+time.sleep(4)
 # Created by /u/whaliam
 # 6Ep96ck9@protonmail.com
